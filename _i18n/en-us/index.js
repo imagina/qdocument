bd2818a0b9b3b212e90b7526f6a759041792b67a
hrodriguezimagina <hrodriguez@imaginacolombia.com>
2024-06-26 12:35:21 -0500

    + added translations

--- _i18n/en-us/index.js
+++ _i18n/en-us/index.js
@@ -1,7 +1,20 @@
-import layout from 'modules/qdocument/_i18n/en-us/layout'
-import sidebar from 'modules/qdocument/_i18n/en-us/sidebar'
-
 export default {
-  layout,
-  sidebar
+  "idocs": {
+    "cms": {
+      "sidebar": {
+        "adminGroup": "Documents",
+        "adminDocuments": "List Documents",
+        "adminCategories": "Document Categories",
+        "panelDocuments": "My Documents"
+      },
+      "newDocument": "New Document",
+      "updateDocument": "Update Document",
+      "newCategory": "New Category",
+      "updateCategory": "Update Category",
+      "documentation": {
+        "documents": "The documents that are published with this module have extra functionalities when it comes to controlling which users, user groups or roles can have access to these both in your CMS and on your WEB site",
+        "categories": "It is possible to create categories, with a name, description and organize them in tree mode (parent categories and child categories) in order to categorize all your documents."
+      }
+    }
+  }
 }
--- _i18n/en-us/sidebar.js
+++
@@ -1,6 +0,0 @@
-export default {
-  adminGroup : 'Documents',
-  adminDocuments : 'List Documents',
-  adminCategories : 'Document Categories',
-  panelDocuments : 'My Documents'
-}
